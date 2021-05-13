ed710f2cc405df0207fcd12337ed75bb4f77a932
sunny <sunny.bikersedge@gmail.com>
2021-05-13 13:23:07 +0100

    Changes as per new ec2 linux instance

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#java -jar /home/ec2-user/table-lock-test-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
-java -jar /home/ubuntu/table-lock-test-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+java -jar /home/ec2-user/table-lock-test-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+#java -jar /home/ubuntu/table-lock-test-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &

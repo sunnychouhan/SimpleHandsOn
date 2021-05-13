0f7a24f5c7208c01777d6008a1171849cfd09b3a
sunny <sunny.bikersedge@gmail.com>
2021-05-13 08:39:38 +0100

    Path fix for ubuntu machine

--- scripts/fix_previleges.sh
+++ scripts/fix_previleges.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-chmod +x /home/ec2-user/justiceleague-tracker-0.0.1-SNAPSHOT.jar
-chmod +x /home/ec2-user/start_server.sh
-chmod +x /home/ec2-user/stop_server.sh
+#chmod +x /home/ec2-user/justiceleague-tracker-0.0.1-SNAPSHOT.jar
+#chmod +x /home/ec2-user/start_server.sh
+#chmod +x /home/ec2-user/stop_server.sh
+
+chmod +x /home/ubuntu/justiceleague-tracker-0.0.1-SNAPSHOT.jar
+chmod +x /home/ubuntu/start_server.sh
+chmod +x /home/ubuntu/stop_server.sh

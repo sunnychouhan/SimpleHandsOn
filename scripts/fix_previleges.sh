0cf0ec2e64310146294ceab6286eede1ae9edecc
sunny <sunny.bikersedge@gmail.com>
2021-05-13 10:19:32 +0100

    test

--- scripts/fix_previleges.sh
+++ scripts/fix_previleges.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#chmod +x /home/ec2-user/justiceleague-tracker-0.0.1-SNAPSHOT.jar
+#chmod +x /home/ec2-user/table-lock-test-0.0.1-SNAPSHOT.jar
 #chmod +x /home/ec2-user/start_server.sh
 #chmod +x /home/ec2-user/stop_server.sh
-sudo chmod +x /home/ubuntu/justiceleague-tracker-0.0.1-SNAPSHOT.jar
+sudo chmod +x /home/ubuntu/table-lock-test-0.0.1-SNAPSHOT.jar
 sudo chmod +x /home/ubuntu/start_server.sh
 sudo chmod +x /home/ubuntu/stop_server.sh
